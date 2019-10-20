c50a766956a895859689ee540cbc8ab108624cb2
gwyrwch <gwyrwch@gmail.com>
2019-10-20 14:38:16 +0300

    TagToNoteViewModel added

--- app/src/main/java/com/example/notes/ViewModels/TagToNoteViewModel.java
+++ app/src/main/java/com/example/notes/ViewModels/TagToNoteViewModel.java
@@ -1,4 +1,41 @@
 package com.example.notes.ViewModels;
 
-public class TagToNoteViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.notes.Models.Note;
+import com.example.notes.Models.Tag;
+import com.example.notes.Models.TagToNote;
+import com.example.notes.Repositories.TagToNoteRepository;
+
+import java.util.List;
+
+public class TagToNoteViewModel extends AndroidViewModel {
+    private TagToNoteRepository tagToNoteRepository;
+
+    private LiveData<List<Tag>> tagsFromNote;
+    private LiveData<List<Note>> notesByTag;
+
+    public TagToNoteViewModel(@NonNull Application application, int tagId, int noteId) {
+        super(application);
+
+        tagToNoteRepository = new TagToNoteRepository(application, tagId, noteId);
+        tagsFromNote = tagToNoteRepository.getTagsFromNote();
+        notesByTag = tagToNoteRepository.getNotesByTag();
+    }
+
+    public LiveData<List<Tag>> getTagsFromNote() {
+        return tagsFromNote;
+    }
+
+    public LiveData<List<Note>> getNotesByTag(){
+        return notesByTag;
+    }
+
+    public void insert(TagToNote tagToNote) {
+        tagToNoteRepository.insert(tagToNote);
+    }
 }
