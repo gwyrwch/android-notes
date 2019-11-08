7de5ec7c69577f197900c60020d4b4079c7a4cf0
gwyrwch <gwyrwch@gmail.com>
2019-11-08 13:07:45 +0300

    small fixes (remove unused code)

--- app/src/main/java/com/example/notes/Database/TagDao.java
+++ app/src/main/java/com/example/notes/Database/TagDao.java
@@ -6,7 +6,6 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
-import com.example.notes.Models.Note;
 import com.example.notes.Models.Tag;
 
 import java.util.List;
--- app/src/main/java/com/example/notes/Database/TagToNoteDao.java
+++ app/src/main/java/com/example/notes/Database/TagToNoteDao.java
@@ -6,7 +6,6 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
-import com.example.notes.Models.AdaptedNote;
 import com.example.notes.Models.Note;
 import com.example.notes.Models.Tag;
 import com.example.notes.Models.TagToNote;
